5b056b5a7eb2c14d93829f7699a61f9a96647c8d
Thiago Giacometi <thiagogiacometi@gmail.com>
2022-11-21 22:04:20 -0300

    teste

--- Desafios/Nivel 03/Desafio 03 - Lista de Tarefas/index.js
+++ Desafios/Nivel 03/Desafio 03 - Lista de Tarefas/index.js
@@ -7,8 +7,11 @@ const checkButton = document.getElementsByClassName("check")
 function startApp() {
 
   for (let el of checkButton) {
-    el.addEventListener('click', function () {
 
+    console.log(checkButton)
+
+    el.addEventListener('click', function () {
+       console.log(`inicio`)
       const parent = this.parentNode.parentNode
 
       if (this.getAttribute(`src`) == `./assets/Frame.svg`) {
@@ -16,28 +19,26 @@ function startApp() {
         this.setAttribute('src', './assets/check.svg');
         parent.classList.add("tasks-finished")
 
-      } else {
-        const parent = this.parentNode.parentNode
+      } else if(this.getAttribute(`src`) != `./assets/Frame.svg`){
 
         this.setAttribute('src', './assets/Frame.svg');
         parent.classList.remove("tasks-finished")
-      }
+
+      }      
 
     });
   }
 
   for (let el of deleteButton) {
     el.addEventListener("click", function () {
-
       const deleteParent = this.parentNode
-      board.removeChild(deleteParent)
-
+      deleteParent.remove()
+      console.log(`remove`)
     })
   }
 }
 
-button.addEventListener("click", () => {
-
+function addTask() {
   const newTask = document.createElement("li")
   newTask.classList = `tasks`
   newTask.innerHTML = `<p class="task"><img class="check pointer" src="./assets/Frame.svg" alt="">${input.value} <img
@@ -49,9 +50,17 @@ button.addEventListener("click", () => {
   input.value = ``
 
   startApp();
-});
+}
+
+
+
+
 
 startApp();
+
+button.addEventListener("click", addTask);
+
+
 
 
 
